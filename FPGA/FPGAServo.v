490f8763e2cef14a3a5c0407a826e1ba9d95ad86
Oskar Weigl <oskar@weigl.nu>
2015-05-03 20:01:10 +0100

    Fixed pin mapping file, blinky works

--- FPGA/FPGAServo.v
+++ FPGA/FPGAServo.v
@@ -1,61 +1,75 @@
 module FPGAServo(
-		input  wire	CLK_50,
-		inout  wire	[38:1] GPIO,
-		input  wire M0_ENC_A,
-		input  wire M0_ENC_B,
-		input  wire M0_ENC_Z,
-		output wire M0_LEDB,
-		output wire M0_LEDG,
-		output wire M0_LEDR,
-		output wire M0_PWM_AH,
-		output wire M0_PWM_AL,
-		output wire M0_PWM_BH,
-		output wire M0_PWM_BL,
-		output wire M0_PWM_CH,
-		output wire M0_PWM_CL,
-		input  wire M1_ENC_A,
-		input  wire M1_ENC_B,
-		input  wire M1_ENC_Z,
-		output wire M1_LEDB,
-		output wire M1_LEDG,
-		output wire M1_LEDR,
-		output wire M1_PWM_AH,
-		output wire M1_PWM_AL,
-		output wire M1_PWM_BH,
-		output wire M1_PWM_BL,
-		output wire M1_PWM_CH,
-		output wire M1_PWM_CL,
-		input  wire M2_ENC_A,
-		input  wire M2_ENC_B,
-		input  wire M2_ENC_Z,
-		output wire M2_LEDB,
-		output wire M2_LEDG,
-		output wire M2_LEDR,
-		output wire M2_PWM_AH,
-		output wire M2_PWM_AL,
-		output wire M2_PWM_BH,
-		output wire M2_PWM_BL,
-		output wire M2_PWM_CH,
-		output wire M2_PWM_CL,
-		input  wire STATUSLED_B,
-		input  wire STATUSLED_R,
-		inout  wire DEV_OE
+		input wire CLK_50,
+		input wire ENC_A1,
+		input wire ENC_A2,
+		input wire ENC_A3,
+		input wire ENC_B1,
+		input wire ENC_B2,
+		input wire ENC_B3,
+		input wire ENC_Z1,
+		input wire ENC_Z2,
+		input wire ENC_Z3,
+		inout wire [30:5] GPIO,
+		//output wire LEDB1,
+		//output wire LEDB2,
+		//output wire LEDB3,
+		//output wire LEDG1,
+		//output wire LEDG2,
+		//output wire LEDG3,
+		//output wire LEDR1,
+		//output wire LEDR2,
+		//output wire LEDR3,
+		//output wire PWM_AH1,
+		//output wire PWM_AH2,
+		//output wire PWM_AH3,
+		//output wire PWM_AL1,
+		//output wire PWM_AL2,
+		//output wire PWM_AL3,
+		//output wire PWM_BH1,
+		//output wire PWM_BH2,
+		//output wire PWM_BH3,
+		//output wire PWM_BL1,
+		//output wire PWM_BL2,
+		//output wire PWM_BL3,
+		//output wire PWM_CH1,
+		//output wire PWM_CH2,
+		//output wire PWM_CH3,
+		//output wire PWM_CL1,
+		//output wire PWM_CL2,
+		//output wire PWM_CL3,
+		input wire RXD,
+		inout wire [36:31] SGPIO,
+		//output wire STATUSLED_B,
+		//output wire STATUSLED_R,
+		//output wire TXD,
+		//output wire CTS_n,
+		input wire RTS_n
 	);
 	
 	//force -freeze sim:/qsystem_tb/qsystem_inst/altpll_0/areset St0 0
 	//force -freeze sim:/qsystem_tb/qsystem_inst/altpll_0/read St0 0
 	//force -freeze sim:/qsystem_tb/qsystem_inst/altpll_0/write St0 0
 
-	qsystem u0 (
+	 qsystem u0 (
 		.clk_clk               (CLK_50),               //             clk.clk
 		.reset_reset_n         (1),         //           reset.reset_n
-		.pwm_0_pwmout_ldrive   ({M0_PWM_AL, M0_PWM_BL, M0_PWM_CL}),   //                .ldrive
-		.pwm_0_pwmout_udrive   ({M0_PWM_AU, M0_PWM_BU, M0_PWM_CU}),   //    pwm_0_pwmout.udrive
-		.qei_0_encoderin_encab ({M0_ENC_A, M0_ENC_B}), // qei_0_encoderin.encab
-		.altpll_0_areset_conduit_export    (0),    //    altpll_0_areset_conduit.export
-        .altpll_0_locked_conduit_export    (),    //    altpll_0_locked_conduit.export
-        .altpll_0_phasedone_conduit_export ()  // altpll_0_phasedone_conduit.export
-	);
+		.pwm_0_pwmout_ldrive   ({GPIO[5], GPIO[7], GPIO[9]}),   //                .ldrive
+		.pwm_0_pwmout_udrive   ({GPIO[6], GPIO[8], GPIO[10]}),   //    pwm_0_pwmout.udrive
+		.qei_0_encoderin_encab ({ENC_A2, ENC_B2})
+	 );
+
+
+//	reg [32:0] ctr = 0;
+//	reg blinkystate = 0;
+//	always @(posedge CLK_50) begin : proc_blinky
+//		ctr <= ctr + 1;
+//		if(ctr == 25000000) begin
+//			blinkystate <= ~blinkystate;
+//			ctr <= 0;
+//		end
+//	end
+//
+//	assign LEDB1 = blinkystate;
 
 endmodule
 	
